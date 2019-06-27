10c9319e8df6f35ccd5732be8cf38f647b6ae41d
Izak <izak@halcyon.nu>
2019-06-27 13:20:57 -0700

    s2idle works now

--- nixos/SB2.nix
+++ nixos/SB2.nix
@@ -42,16 +42,6 @@
 
   services.udev.packages = [ pkgs.SB2_firmware pkgs.libwacom ];
 
-  powerManagement = {
-    enable = true;
-  };
-
-  services.acpid = {
-    enable = true;
-    handlers = {
-      lid = { action = ""; event = "button/lid.*"; };
-    };
-  };
 
   services.xserver.videoDrivers = [ "intel" ];
   #services.xserver.videoDrivers = [ "nouveau" ];
@@ -75,7 +65,6 @@
     };
   };
  
-  services.xserver.xkbOptions = "caps:hyper";
   
   networking.networkmanager = {
     enable = true;
@@ -88,65 +77,39 @@
 
   environment.etc = { "systemd/sleep.conf".text = "SuspendState=freeze\n"; };
 
+  powerManagement = {
+    enable = true;
+  };
+
+  services.acpid = {
+    enable = true;
+    #handlers = {
+      #lid = { action = ""; event = "button/lid.*"; };
+    #};
+  };
+
   powerManagement.powerDownCommands = ''
     source /etc/profile
+    systemctl stop wpa_supplicant.service;
     systemctl stop home-izak-.org_sync.mount
-    systemctl stop wpa_supplicant.service
-    modprobe -r mwlwifi
-    modprobe -r intel_ipts
-    modprobe -r mei_me
-    modprobe -r mei
-    modprobe -r mwifiex_pcie;
-    modprobe -r mwifiex;
-    modprobe -r cfg80211;
-  '';
+  '' +
+    (builtins.readFile ./remove_modules); 
 
-    #acpitool -W 21 >2 /dev/null
-    #acpitool -W 5 >2 /dev/null
     #echo disabled > /sys/devices/LNXSYSTM:00/LNXSYBUS:00/PNP0C0D:00/power/wakeup
-    #acpitool -W 2 >2 /dev/null
+
   powerManagement.powerUpCommands = ''
     source /etc/profile
-    modprobe -r intel_ipts
-    modprobe -r mwlwifi
-    modprobe -r mei_me
-    modprobe -r mei
-    modprobe -r mwifiex_pcie;
-    modprobe -r mwifiex;
-    modprobe -r cfg80211;
-    # and reload
-    modprobe -i intel_ipts
-    modprobe -i mei_me
-    modprobe -i mei
-    modprobe -i cfg80211;
-    modprobe -i mwifiex;
-    modprobe -i mwifiex_pcie;
-    modprobe -i mwlwifi
     echo 1 > /sys/bus/pci/rescan
-    systemctl restart wpa_supplicant.service
-    modprobe -r soc_button_array
-    modprobe -i soc_button_array
+    acpitool -W 2 >2 /dev/null
   '';
   powerManagement.resumeCommands = ''
     source /etc/profile
-    modprobe -r intel_ipts
-    modprobe -r mwlwifi
-    modprobe -r mei_me
-    modprobe -r mei
-    modprobe -r mwifiex_pcie;
-    modprobe -r mwifiex;
-    modprobe -r cfg80211;
-    modprobe -r soc_button_array
-    # and reload
-    modprobe -i intel_ipts
-    modprobe -i mei_me
-    modprobe -i mei
-    modprobe -i cfg80211;
-    modprobe -i mwifiex;
-    modprobe -i mwifiex_pcie;
-    modprobe -i mwlwifi
-    modprobe -i soc_button_array
+  '' +
+  (builtins.readFile ./remove_modules) +
+  (builtins.readFile ./insert_modules) + 
+  ''
     echo 1 > /sys/bus/pci/rescan
+    acpitool -W 2 >2 /dev/null
     systemctl restart wpa_supplicant.service
     systemctl start home-izak-.org_sync.mount
   '';
